d2e6926cd86200f29417db64664f751b72e5ebe8
cristianbgp <cristian.granda.pastor@gmail.com>
2019-04-22 20:23:20 -0500

    Update series controller

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -28,6 +28,7 @@ class SeriesController < ApplicationController
     authorize(Serie)
     @serie = Serie.new(serie_params)
     if @serie.save
+      SerieMailer.with(user: current_user, serie: @serie).serie_created.deliver_later
       redirect_to series_path(@serie), notice: 'Serie was successfully created.'
     else
       render :new
@@ -38,6 +39,7 @@ class SeriesController < ApplicationController
     authorize(Serie)
     @serie = Serie.find(params[:id])
     if @serie.update(serie_params)
+      SerieMailer.with(user: current_user, serie: @serie).serie_updated.deliver_later
       redirect_to series_path(@serie), notice: 'Serie was successfully updated.'
     else
       render :edit
@@ -47,6 +49,7 @@ class SeriesController < ApplicationController
   def destroy
     authorize(Serie)
     @serie.destroy
+    SerieMailer.with(user: current_user, serie: @serie).serie_deleted.deliver_now
     redirect_to series_index_path, notice: 'Serie was successfully destroyed.'
   end
 
